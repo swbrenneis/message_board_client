5a215dfad990ad84a5c215aa6a87186ce7cee181
Steve Brenneis <swbrenneis@gmail.com>
2025-10-06 16:31:54 -0400

    Updated JSON values

--- delete_enclave.py
+++ delete_enclave.py
@@ -2,28 +2,34 @@ import requests
 
 import crypto_util
 
-data_file = input("Enter data file name: ")
+screen_name = input("Enter screen name: ")
+signing_password = input('Enter signing password: ')
+
 # Load the user details
 lines = []
-with open(data_file, 'r') as file:
+with open(f'{screen_name}_data', 'r') as file:
     for line in file.readlines():
         lines.append(line)
 
-screen_name = lines[0].strip()
 public_id = lines[1].strip()
-enclave_key = lines[2].strip()
+
+# Load the session details
+session_lines = []
+with open('session_data', 'r') as file:
+    for line in file.readlines():
+        session_lines.append(line)
+
+session_id = session_lines[1].strip()
 
 to_hash = [
+    session_id,
     screen_name,
     public_id,
-    enclave_key,
 ]
-
-signing_password = input('Enter signing password: ')
 signing_private_key = crypto_util.private_key_from_file('signing_private_key.pem', signing_password)
 signature = crypto_util.sign_message(to_hash, signing_private_key)
 
-initiate_deletion = {'screenName': screen_name, 'publicId': public_id, 'enclaveKey': enclave_key, 'signature': signature}
+initiate_deletion = {'sessionId': session_id, 'screenName': screen_name, 'publicId': public_id, 'signature': signature}
 response = requests.post('http://localhost:8446/enclave/delete', json=initiate_deletion)
 deleted = response.json()
 
