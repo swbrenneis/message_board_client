81549d4604b2fac6c38557fe417c1f3baf363ad6
Steve Brenneis <swbrenneis@foronlyyou.com>
2025-10-06 22:01:40 -0400

    Troubleshooting

--- delete_enclave.py
+++ delete_enclave.py
@@ -1,3 +1,5 @@
+import base64
+
 import requests
 
 import crypto_util
@@ -27,10 +29,18 @@ to_hash = [
     public_id,
 ]
 signing_private_key = crypto_util.private_key_from_file('signing_private_key.pem', signing_password)
+signing_public_key = signing_private_key.public_key()
+public_numbers = signing_public_key.public_numbers()
+x,y = public_numbers.x, public_numbers.y
 signature = crypto_util.sign_message(to_hash, signing_private_key)
+signature_string = signature.decode('utf-8')
 
-initiate_deletion = {'sessionId': session_id, 'screenName': screen_name, 'publicId': public_id, 'signature': signature}
+initiate_deletion = {'sessionId': session_id, 'screenName': screen_name, 'publicId': public_id, 'signature': signature_string}
 response = requests.post('http://localhost:8446/enclave/delete', json=initiate_deletion)
+if (response.status_code != 200):
+    print(f'{response.status_code}: {response.text}')
+    quit()
+
 deleted = response.json()
 
 if deleted['deleted']:
